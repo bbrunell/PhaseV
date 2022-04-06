f02879bad71d3df1a5397efc0ad87415115108f8
Noah Abrigo <noahabrigo@gmail.com>
2022-04-05 21:22:35 -0400

    Delete Functions.js

--- Functions.js
+++
@@ -1,7 +0,0 @@
-function fn(){
-    var Name = document.getElementById('name').value;
-    document.getElementById('result').innerHTML += '<br>' + Name;
-}
-function clearContent(){
-    document.getElementById('result').innerHTML = '';
-}
